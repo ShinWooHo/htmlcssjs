8c83766fc13733d35c4698348bd65e8e5cad5c7c
신우호 <@DESKTOP-E2VSQGP>
2024-04-06 15:56:20 +0900

    수정

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -10,7 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	@RequestMapping("/")
 	public String index() {
-		log.info("실행");
+		log.info("실행1");
+		log.info("실행2");
 		return "home";
 	}
 }
